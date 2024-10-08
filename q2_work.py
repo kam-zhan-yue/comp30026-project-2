834a75d5cadfe95fd8c1780e650cff16aa025409
kamzhanyue <kamzhanyue@gmail.com>
2024-10-09 01:43:45 +1100

    Finalise Q2b

--- q2_work.py
+++ q2_work.py
@@ -64,11 +64,52 @@ inputs = {
     '01': False,
 }
 
+class NFAWrapper:
+    def __init__(self, initial_state: str, final_states: set[str]):
+        self.states = set()
+        self.input_symbols = set()
+        self.transitions = {}
+        self.initial_state = initial_state
+        self.final_states = final_states
+    
+    def add_transition(self, state_1, i, state_2):
+        # ensure that it is an initialised dictionary
+        if state_1 not in self.transitions:
+            self.transitions[state_1] = {}
+        # add the new transition into the nfa transition dictionary
+        if i in self.transitions[state_1]:
+            self.transitions[state_1][i].add(state_2)
+        else:
+            self.transitions[state_1][i] = {state_2}
+
+    def add_transitions(self, dfa_transitions):
+        for state, transition in dfa_transitions.items():
+            for i, t in transition.items():
+                # add the new transition into the nfa transition dictionary
+                self.add_transition(state, i, t)
+
+    def update_dfa(self, dfa: DFA):
+        self.states.update(dfa.states)
+        self.input_symbols.update(dfa.input_symbols)
+        self.add_transitions(dfa.transitions)
+    
+    def get_nfa(self) -> NFA:
+        return NFA(
+            states=self.states,
+            input_symbols=self.input_symbols,
+            transitions=self.transitions,
+            initial_state=self.initial_state,
+            final_states=self.final_states,
+        )
+
 def suffix_item(thing, suffix):
     return f'{thing}_{suffix}'
 
 def suffix_list(original, suffix):
     return [suffix_item(thing, suffix) for thing in original]
+
+def suffix_set(original, suffix):
+    return set(suffix_list(original, suffix))
 
 def suffix_transitions(original, suffix):
     new_transitions = {}
@@ -79,43 +120,44 @@ def suffix_transitions(original, suffix):
             new_transitions[suffixed][i] = suffix_item(t, suffix)
     return new_transitions
 
-def add_transitions(nfa_transitions, dfa_transitions):
-    for state, transition in dfa_transitions.items():
-        for i, t in transition.items():
-            # check if the nfa transition dictionary is empty
-            if state not in nfa_transitions:
-                nfa_transitions[state] = {}
-            
-            # add the new transition into the nfa transition dictionary
-            if i in nfa_transitions[state]:
-                nfa_transitions[state][i].add(t)
-            else:
-                nfa_transitions[state][i] = {t}
+def clone(dfa: DFA, suffix: str) -> DFA:
+    return DFA(
+        states=suffix_set(dfa.states, suffix),
+        input_symbols=dfa.input_symbols,
+        transitions=suffix_transitions(dfa.transitions, suffix),
+        initial_state=suffix_item(dfa.initial_state, suffix),
+        final_states=suffix_set(dfa.final_states, suffix),
+    )
 
 def q2b(a: DFA, b: DFA) -> NFA:
-    states = set(a.states)
-    other_states = set(b.states)
-    states.update(other_states)
+    # Create a secondary a and a new base for b
+    a_2 = clone(a, '2')
+    b_base = clone(b, 'base')
 
-    # for every state in 
-    # first clone a into a2
-    # need to combine the input symbols, make sure they are updated
-    a2_states = suffix_list(a.states, 2)
-    a2_transitions = suffix_transitions(a.transitions, 2)
+    # Initialse the NFA's parameters
+    nfa_wrapper = NFAWrapper(a.initial_state, a_2.final_states)
+    nfa_wrapper.update_dfa(a)
+    nfa_wrapper.update_dfa(a_2)
 
     suffix = 0
-    nfa_transitions = {}
-    add_transitions(nfa_transitions, a.transitions)
-    add_transitions(nfa_transitions, a2_transitions)
+    for state in a.states:
+        # Create a clone of b_base
+        b_branch = clone(b_base, suffix)
+
+        nfa_wrapper.update_dfa(b_branch)
+
+        # Create an epsilon transition from the current state to the start state of the branch
+        nfa_wrapper.add_transition(state, '', b_branch.initial_state)
+
+        # Create an epsilon transition from the final state of the branch to the corresponding state of a2
+        for final_state in b_branch.final_states:
+            nfa_wrapper.add_transition(final_state, '', suffix_item(state, '2'))
+        suffix += 1
+    
+    return nfa_wrapper.get_nfa()
 
 
+nfa = q2b(dfa1, dfa2)
 
-    for state, transitions in a.transitions.items():
-        branch = suffix_transitions(b.transitions, suffix)
-
-        print(state)
-
-    print(nfa_transitions)
-    return None
-
-q2b(dfa1, dfa2)
+# Testing for sanity
+nfa.show_diagram().draw('q2b.png')
